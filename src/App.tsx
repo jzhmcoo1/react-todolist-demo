d94201c2825080f200cae7ed9afcd4a1b16c9c43
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-11-14 18:58:34 +0800

    chore: 引入antd库

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { Layout } from "antd";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainSection from "./components/MainSection";
+import { TodoProvider } from "./utils/context";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainSection />
-      <Footer />
+    <div>
+      <TodoProvider>
+        <Layout.Header>
+          <Header />
+        </Layout.Header>
+        <Layout.Content>
+          <MainSection />
+        </Layout.Content>
+        <Layout.Footer>
+          <Footer />
+        </Layout.Footer>
+      </TodoProvider>
     </div>
   );
 }
